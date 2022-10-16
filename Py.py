3c900c1f83ca3858e5497b5715260d5f539f9534
Kamal Sharma <115937888+kkKamalkk@users.noreply.github.com>
2022-10-16 20:40:03 +0530

    Add files via upload

---
+++ Py.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ('hello')
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[8]:
+
+
+pd.read_excel(r'C:\Users\kamal\Desktop\Data Trained\datapanda.xlsx')
+
+
+# In[9]:
+
+
+import os
+
+
+# In[11]:
+
+
+pd.read_csv(r'C:\Users\kamal\Desktop\Data Trained\Datapanda1.csv')
+
+
+# In[18]:
+
+
+pd.read_csv(r'C:\Users\kamal\Desktop\Data Trained\datapandatsv.tsv',sep='/t')
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+pd.read
+
+
+# In[23]:
+
+
+pd.read_csv(r'C:\Users\kamal\Desktop\Data Trained\datapandatsv.tsv',sep='\t')
+
+
+# In[ ]:
+
+
+
+
